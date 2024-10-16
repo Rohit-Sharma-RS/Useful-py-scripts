18685e03dfd98ebd31392443a8b6b6cb3c26c224
Rohit-Sharma-RS <rohitrnc5458@gmail.com>
2024-10-16 12:59:23 +0530

    removed libtorrent usage

--- torrent downloader/torrent.py
+++ torrent downloader/torrent.py
@@ -1,27 +1,9 @@
-import libtorrent as lt
-import time
+import subprocess
 
-def download_torrent(torrent_link):
-    ses = lt.session()
-    params = {
-        'save_path': './downloads/',
-        'storage_mode': lt.storage_mode_t(2),
-    }
-    handle = lt.add_magnet_uri(ses, torrent_link, params)
-    ses.start_dht()
+def download_torrent(magnet_link):
+    process = subprocess.Popen(['webtorrent', 'download', magnet_link, '--out', './downloads'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    for line in process.stdout:
+        print(line.decode(), end='')
 
-    print("Downloading...")
-    while not handle.has_metadata():
-        time.sleep(1)
-    print("Metadata acquired, starting download...")
-
-    while handle.status().state != lt.torrent_status.seeding:
-        s = handle.status()
-        print(f'Downloading: {s.progress * 100:.2f}% complete (down: {s.download_rate / 1000:.2f} kB/s up: {s.upload_rate / 1000:.2f} kB/s peers: {s.num_peers})')
-        time.sleep(5)
-
-    print("Download complete!")
-
-
-torrent_link = input("Enter the torrent link: ")
-download_torrent(torrent_link)
+magnet_link = input('Enter magnet link: ')
+download_torrent(magnet_link)
